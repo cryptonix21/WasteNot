5d7d3cec98a57942158e2563ab283a30e003ec42
Gopal Gurram <gopalgurram1@gmail.com>
2024-12-19 12:16:57 +0530

    add the verce.json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+      }
+    ];
+  }
 };
 
 export default nextConfig;
