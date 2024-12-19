b83f9df9c4c5c02a3b60e76c9a414bf2d000beee
Gopal Gurram <gopalgurram1@gmail.com>
2024-12-19 13:30:03 +0530

    removed the unused variables and folders

--- .history/frontend/app/components/LocationPicker_20241219125638.tsx
+++ .history/frontend/app/components/LocationPicker_20241219125638.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
 
 const loader = new Loader({
-  apiKey: process.env.GOOGLE_MAPS_API_KEY || '',
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
   version: 'weekly',
   libraries: ['places']
 });
